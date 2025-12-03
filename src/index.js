a5c825787ca13adb6ec95ad891714eef3a427a6b
Lefteris Lemontzoglou <lefteris11112@gmail.com>
2025-12-03 21:27:43 +0200

    Danger Zones + fix map not fitting properly

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import BoatNavigation from './BoatNavigation';
 import './index.css';
 import AdminPanel from "./components/AdminPanel";
 import Layout from "./Layout";
+import ScrollToTop from "./components/ScrollToTop";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 {/* Pages WITH navbar */}
                 <Route element={<Layout />}>
